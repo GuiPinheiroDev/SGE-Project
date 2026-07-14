9b0f96ed5c4f65a68da880cd7668f74cdd671512
Guilherme Pinheiro <guipinheiro2501@gmail.com>
2026-07-14 19:44:22 -0300

    Configurações iniciais

--- src/main/java/br/com/sge/windows/SGE_Project.java
+++ src/main/java/br/com/sge/windows/SGE_Project.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package br.com.sge.windows;
 
-/**
- *
- * @author User
- */
 public class SGE_Project {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
     }
 }
