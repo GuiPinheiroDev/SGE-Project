969746303f9c4bd49a580e7dd4ec2cb66901f6e7
Guilherme Pinheiro <guipinheiro2501@gmail.com>
2026-07-16 21:22:22 -0300

    Criando a classe itemVenda

---
+++ src/main/java/br/com/sge/ItemVenda.java
@@ -0,0 +1,55 @@
+package br.com.sge;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import java.math.BigDecimal;
+
+@Entity
+public class ItemVenda {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    
+    private int quantidade;
+    private BigDecimal valorUnitario;
+    
+    @ManyToOne
+    private Produto produto;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public BigDecimal getValorUnitario() {
+        return valorUnitario;
+    }
+
+    public void setValorUnitario(BigDecimal valorUnitario) {
+        this.valorUnitario = valorUnitario;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+    
+    
+}
